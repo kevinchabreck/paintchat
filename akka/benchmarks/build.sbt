2b8a36511f3daf244d8240c5d5c266e529364c80
John McCrone <jvm3487@gmail.com>
2015-10-26 19:42:34 -0400

    Basic infrastructure in place for test clients.

--- akka/benchmarks/build.sbt
+++ akka/benchmarks/build.sbt
@@ -6,9 +6,5 @@ version := "1.0"
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
-  	"com.typesafe.akka" % "akka-actor_2.11" % "2.3.9",
-	"io.spray" %% "spray-can" % "1.3.3",
-	"com.wandoulabs.akka" %% "spray-websocket" % "0.1.4",
-	"io.spray" %%  "spray-json" % "1.3.1",
 	"org.java-websocket" % "Java-WebSocket" % "1.3.0"
 )
---
+++ akka/benchmarks/src/main/scala/benchmarks/BenchMarks.scala
@@ -0,0 +1,47 @@
+package benchmarks
+
+import org.java_websocket.client.WebSocketClient
+import org.java_websocket.drafts.{Draft_17}
+import org.java_websocket.handshake.ServerHandshake
+import java.net.URI
+
+object Benchmarks extends App {
+
+  class TestClient(id: Int, delay: Long) extends WebSocketClient(new URI(s"ws://localhost:8080/"), new Draft_17){
+    override def onMessage(message: String): Unit = {
+      Thread.sleep(delay);
+      println("Delay is " + delay)
+    }
+    override def onClose(code: Int, reason: String, remote: Boolean): Unit = println("This is being closed!")
+    override def onOpen(handshakedata: ServerHandshake): Unit = println(s"There is a websocket opened and the delay is $delay")
+    override def onError(ex: Exception): Unit = println("Ahh, I am in error! " + ex)
+  }
+
+  def doMain() {
+    //implicit val system = ActorSystem()
+    //val clientManager:  = system.actorOf(Props[TestClient], "
+    val clientMap = collection.mutable.Map[TestClient, Int]()
+
+    val numberClients = 10
+    val randomRange = 100
+    val base = 50
+    (1 to numberClients).foreach({ cnt =>
+      val client = new TestClient(cnt, Math.round(Math.random() * randomRange + base))
+      Thread.sleep(10)
+      println("I am here " + cnt)
+      client.connectBlocking()
+      client.send("GETBUFFER:")
+      client.send("USERNAME:" + cnt)
+      clientMap(client) = cnt
+    })
+
+    readLine("Hit ENTER to exit ...\n")
+    println("Shutting down benchmark framework")
+    
+    clientMap.foreach({ case (client, clientNum) =>
+      client.close()
+    })
+  }
+
+  doMain()
+}
