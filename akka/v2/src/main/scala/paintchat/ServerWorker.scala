ebb9a7cfc8737a7f535fc724c7b2fa9344c6cce7
Kevin Chabreck <kevin.chabreck@gmail.com>
2015-11-18 19:37:06 -0500

    updated server status update protocol

--- akka/v2/src/main/scala/paintchat/ServerWorker.scala
+++ akka/v2/src/main/scala/paintchat/ServerWorker.scala
@@ -18,46 +18,27 @@ case class UserJoin(username: String, client:ActorRef) extends ServerUpdate
 case class UserLeft(username: String) extends ServerUpdate
 case class UserCount(usercount: Int) extends ServerUpdate
 case class PaintBuffer(pbuffer: ListBuffer[String]) extends ServerUpdate
-case class UserList(userlist: ListBuffer[String]) extends ServerUpdate
-case class UpdatedBuffer(pbuffer: ListBuffer[String], userlist: ListBuffer[String], usercount: Int) extends ServerUpdate
+case class UserList(userlist: Iterable[String]) extends ServerUpdate
+case class UpdatedBuffer(pbuffer: Iterable[String], userlist: Iterable[String], usercount: Int) extends ServerUpdate
 
 class ServerWorker extends Actor with ActorLogging {
   val clients = new HashMap[ActorRef, String]
   val pbuffer = new ListBuffer[String]
   val userlist = new ListBuffer[String]
-  var usercount = 0
   val mediator = DistributedPubSub(context.system).mediator
   mediator ! Subscribe("update", self)
-  // println(s"I AM ${context.self.path}")
 
   def receive = {
     case SubscribeAck(Subscribe(topic, None, `self`)) => println(s"subscribed to topic: $topic")
     case UpgradedToWebSocket => clients(sender) = ""
     case ServerStatus => sender ! ServerInfo(clients.size)
     case c:Chat => clients.keys.foreach(_ ! c)
-    case UserJoin(username,client) => 
-      clients.keys.foreach(_ ! UserJoin(username,client))
-      userlist += username
-      usercount += 1
-      clients.keys.foreach(_ ! UserCount(usercount))
-
-    case UserLeft(username) => 
-      clients.keys.foreach(_ ! UserLeft(username))
-      userlist -= username
-      usercount -= 1
-      clients.keys.foreach(_ ! UserCount(usercount))
-
     case GetBuffer => sender ! PaintBuffer(pbuffer)
     case GetUserList => sender ! UserList(userlist)
-    case GetUpdated => sender ! UpdatedBuffer(pbuffer,userlist, usercount)
-    case UpdatedBuffer(p,ul,uc) => 
-      p.foreach(x => pbuffer += x)
-      ul.foreach(x => userlist += x)
-      usercount = uc
-
-      // println(s"want to broadcast paintbuffer to all clients")
-      // println(s"clients: ${clients}")
-      // clients.keys.foreach(_ ! BroadcastBuffer(p))
+    case GetUpdated => sender ! UpdatedBuffer(pbuffer, userlist, userlist.size)
+    case UpdatedBuffer(p,ul,uc) =>
+      pbuffer ++= p
+      userlist ++= ul
 
     case Http.Connected(remoteAddress, localAddress) =>
       val connection = context.watch(context.actorOf(ClientWorker.props(sender, self, mediator)))
@@ -79,6 +60,16 @@ class ServerWorker extends Actor with ActorLogging {
       clients(sender) = username
       sender ! Accepted(username)
       mediator ! Publish("update", UserJoin(username,sender))
+
+    case UserJoin(username,client) =>
+      clients.keys.foreach(_ ! UserJoin(username,client))
+      userlist += username
+      clients.keys.foreach(_ ! UserCount(userlist.size))
+
+    case UserLeft(username) =>
+      clients.keys.foreach(_ ! UserLeft(username))
+      userlist -= username
+      clients.keys.foreach(_ ! UserCount(userlist.size))
 
     case x => println(s"[SERVER] recieved unknown message from $sender: $x")
   }
